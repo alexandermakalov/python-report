b0666df6d00422980c7020efb2ab6495a430a4b1
Alexander Makalov <amakalau@exadel.com>
2018-10-09 23:17:59 +0300

    feat: read employees info from file. Just only number and coordinate

---
+++ dev_tmp/Employee.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, coordinate, number):
+        self.coordinate = coordinate
+        self.number = number
+        self.fullName = None
+        self.rate = None
+        self.position = None
--- dev_tmp/test.py
+++ dev_tmp/test.py
@@ -1,14 +1,45 @@
 from openpyxl import load_workbook
+from Employee import Employee
 
 # Load in the workbook
 wb = load_workbook('./python-report-1.xlsx', read_only=True)
 
 # Get sheet names
-print(wb.sheetnames)
+sheets = wb.sheetnames
+print(sheets)
 
-ws = wb['ИЮЛЬ']
+ws = wb[sheets[0]]  # get first sheet
 
-for row in ws.rows:
-    for cell in row:
-        if cell.value is not None:
-            print('address:' + cell.coordinate + ' value:' + str(cell.value))
+print('sheet.max_row ' + str(ws.max_row))
+print('sheet.max_column ' + str(ws.max_column))
+
+employeeNumberColumn = 1
+startEmployeeNumberRow = 14
+def init_employee():
+    endEmployeeTable = 0
+    employees = []
+
+    for i in range(startEmployeeNumberRow, ws.max_row):
+        cell = ws.cell(row=i, column=employeeNumberColumn)
+        if cell.value is None:
+            endEmployeeTable += 1
+        else:
+            endEmployeeTable = 0
+            employee = Employee(cell.coordinate, cell.value)
+            employees.append(employee)
+
+        print(i, cell.value)
+        print('endEmployeeTable', i, endEmployeeTable)
+        if endEmployeeTable == 2:
+            break
+
+    return employees
+
+
+employees = init_employee()
+test = 1
+
+# for row in ws.rows:
+#     for cell in row:
+#         if cell.value is not None:
+#             print('address:' + cell.coordinate + ' value:' + str(cell.value))
